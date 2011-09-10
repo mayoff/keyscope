2b58bba8714a21e17ed8f05a1046dd1c378c1548
Rob Mayoff <git@rob.dqd.com>
2011-09-10 13:44:49 -0500

    detect closed connections; exit when last connection closes

--- keyscope.py
+++ keyscope.py
@@ -15,19 +15,25 @@ class LongPollPage(Resource):
     isLeaf = True
 
     def __init__(self):
-        self.presence = []
+        self.__requests = []
         loopingCall = task.LoopingCall(self.__print_time)
         loopingCall.start(1, False)
         Resource.__init__(self)
 
     def render_GET(self, request):
         request.write('<b>%s</b>' % (time.ctime(),))
-        self.presence.append(request)
+        request.notifyFinish().addErrback(self.__requestTerminated, request)
+        self.__requests.append(request)
         return server.NOT_DONE_YET
 
     def __print_time(self):
-        for p in self.presence:
-            p.write('<b>%s</b>' % (time.ctime(),))
+        for r in self.__requests:
+            r.write('<b>%s</b>' % (time.ctime(),))
+
+    def __requestTerminated(self, err, request):
+        self.__requests.remove(request)
+        if not self.__requests:
+            reactor.stop()
 
 resource = LongPollPage()
 factory = Site(resource)

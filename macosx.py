b3896d8c30afc3eb963d5f10d99b97ad3027e09f
Rob Mayoff <git@rob.dqd.com>
2011-09-13 17:57:35 -0500

    use osascript to enable universal access
    The event tap can only sniff keyup/keydown if universal access is enabled.

--- macosx.py
+++ macosx.py
@@ -8,9 +8,12 @@ from twisted.web.resource import Resource
 from twisted.web import server
 from twisted.internet import task, reactor
 import json
+import subprocess
 
 # Quartz is part of PyObjC, which comes standard on at least Mac OS X 10.6 and 10.7.
 from Quartz import *
+
+subprocess.call(['osascript', '-e', 'tell app "System Events" to set UI elements enabled to true'])
 
 #/System/Library/Frameworks/Carbon.framework/Frameworks/HIToolbox.framework/Headers/Events.h
 keynameForKeycode = ['A', 'S', 'D', 'F', 'H', 'G', 'Z', 'X', 'C', 'V', 'Section', 'B', 'Q', 'W', 'E', 'R', 'Y', 'T', '1', '2', '3', '4', '6', '5', 'Equal', '9', '7', 'Minus', '8', '0', 'RightBracket', 'O', 'U', 'LeftBracket', 'I', 'P', 'Return', 'L', 'J', 'Quote', 'K', 'Semicolon', 'Backslash', 'Comma', 'Slash', 'N', 'M', 'Period', 'Tab', 'Space', 'Grave', 'Delete', '0x34', 'Escape', 'RightCommand', 'Command', 'Shift', 'CapsLock', 'Option', 'Control', 'RightShift', 'RightOption', 'RightControl', 'Function', 'F17', 'KeypadDecimal', '0x42', 'KeypadMultiply', '0x44', 'KeypadPlus', '0x46', 'KeypadClear', 'VolumeUp', 'VolumeDown', 'Mute', 'KeypadDivide', 'KeypadEnter', '0x4d', 'KeypadMinus', 'F18', 'F19', 'KeypadEquals', 'Keypad0', 'Keypad1', 'Keypad2', 'Keypad3', 'Keypad4', 'Keypad5', 'Keypad6', 'Keypad7', 'F20', 'Keypad8', 'Keypad9', 'Yen', 'Underscore', 'KeypadComma', 'F5', 'F6', 'F7', 'F3', 'F8', 'F9', 'Eisu', 'F11', 'Kana', 'F13', 'F16', 'F14', '0x6c', 'F10', '0x6e', 'F12', '0x70', 'F15', 'Help', 'Home', 'PageUp', 'ForwardDelete', 'F4', 'End', 'F2', 'PageDown', 'F1', 'LeftArrow', 'RightArrow', 'DownArrow', 'UpArrow']

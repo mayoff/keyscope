68bf104932c9a0156e404909b829b769135d68f0
Rob Mayoff <git@rob.dqd.com>
2011-09-15 22:39:06 -0500

    use applescript to enable universal access

--- sniffer-macosx/sniffer/main.m
+++ sniffer-macosx/sniffer/main.m
@@ -1,8 +1,27 @@
 #import "Recorder.h"
 
+static void enableUniversalAccess()
+{
+    NSAppleScript *script = [[NSAppleScript alloc] initWithSource:@"tell app \"System Events\"\nset UI elements enabled to true\nget UI elements enabled\nend"];
+    NSDictionary *error;
+    NSAppleEventDescriptor *aed = [script executeAndReturnError:&error];
+    [script release];
+    if (!aed) {
+        NSLog(@"keyscope/sniffer: error enabling universal access: %@", error);
+        exit(1);
+    }
+    if ([aed descriptorType] != 'true') {
+        NSLog(@"keyscope/sniffer: failed to enable universal access");
+        exit(1);
+    }
+}
+
 int main(int argc, const char * argv[])
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    // Try to ensure that access for assistive devices is enabled in the Universal Access system preference pane.  Event taps can't listen to keydown and keyup events without it.
+    enableUniversalAccess();
 
     [[[[Recorder alloc] init] autorelease] run];
 

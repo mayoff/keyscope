6a16439e3f2cb3a4104badecbaeed760c8f5b214
Rob Mayoff <git@rob.dqd.com>
2011-09-20 22:59:21 -0500

    get key flipping working for WebKit; no 3D transforms in Mozilla

--- static/app.js
+++ static/app.js
@@ -71,7 +71,7 @@ module.define('app', function (require, exports) {
             var labelSet = keyboards.labelSetForId(app.model.labelSetId);
             var keyControllers = this.keyControllers;
             for (var name in keyControllers) {
-                keyControllers[name].label = (name in labelSet) ? labelSet[name] : name;
+                keyControllers[name].setLabel((name in labelSet) ? labelSet[name] : name);
             }
         },
 
--- static/keyController.js
+++ static/keyController.js
@@ -6,6 +6,13 @@ module.define('keyController', function (require, exports) {
     var domBinder = require('framework/domBinder');
     var observe = require('framework/observe');
 
+    // Gotta have 3D transforms to bother with a transition.
+    var kTransitionDelayKey = false ? null
+        : ('perspective' in document.body.style) ? 'transitionDelay'
+        : ('webkitPerspective' in document.body.style) ? 'webkitTransitionDelay'
+        : ('MozPerspective' in document.body.style) ? 'MozTransitionDelay'
+        : false;
+
     var gradient = [];
 
     (function makeGradient() {
@@ -43,39 +50,99 @@ module.define('keyController', function (require, exports) {
         this.name = keyDescription.name;
         this.model = app.model;
         this.key = app.model.keyForName(this.name);
-        this.didSeeFirstLabel = false;
+        this.sideShown = 'Front';
 
         var node = this.node = document.createElement('div');
         this.node$ = $(node);
-        node.className = 'key keyup';
+        node.className = 'keyParent keyup';
         var style = node.style;
         style.left = keyDescription.x + 'mm';
         style.top = keyDescription.y + 'mm';
         style.width = keyDescription.width + 'mm';
         style.height = keyDescription.height + 'mm';
-        node.innerHTML = format(
-            '<div class="keylabel" data-bind-innerhtml="label">{name}</div>' +
+        node.innerHTML =
+            '<div class="keylabel keylabelFront" data-bind-innerhtml="labelFront"></div>' +
+            '<div class="keylabel keylabelBack" data-bind-innerhtml="labelBack"></div>' +
             '<div class="keyToolTip">' +
                 'Name: <span data-bind-innerhtml="name"></span><br>' +
                 'Times pressed: <span data-bind-innerhtml="key.pressCount"></span><br>' +
                 'Rank: <span data-bind-innerhtml="key.rank"></span>/<span data-bind-innerhtml="model.maxRank"></span>' +
-            '</div>', this);
-        this.labelNode = node.firstChild;
-        this.labelNode$ = $(this.labelNode);
+            '</div>';
+        this.keylabelFront = node.querySelector('.keylabelFront');
+        this.keylabelBack = node.querySelector('.keylabelBack');
+        if (kTransitionDelayKey) {
+            var transitionDelay = keyDescription.x + 'ms';
+            this.keylabelFront.style[kTransitionDelayKey] = transitionDelay;
+            this.keylabelBack.style[kTransitionDelayKey] = transitionDelay;
+        } else {
+            this['keylabel' + this.sideHidden].style.display = 'none';
+        }
         domBinder.bind(node, this);
         parentNode.appendChild(node);
+        var self = this;
+        window.setTimeout(function () { self.node$.addClass('keylabelShow' + self.sideShown); }, 0);
 
         observe.observePath(this.key, 'state', this.keyStateDidChange, this, observe.kAugmentDestroyMethod);
         observe.observePath(this.key, 'rank', this.rankDidChange, this, observe.kAugmentDestroyMethod);
         observe.observePath(app.model, 'maxRank', this.rankDidChange, this, observe.kAugmentDestroyMethod);
-        observe.observePath(this, 'label', this.labelDidChange, this, observe.kAugmentDestroyMethod);
         this.rankDidChange();
     }
+
+    Object.defineProperty(KeyController.prototype, 'sideHidden', {
+        configurable: true,
+        get: function () {
+            return this.sideShown === 'Front' ? 'Back' : 'Front';
+        }
+    });
 
     KeyController.prototype.destroy = function () {
         this.node.parentNode.removeChild(this.node);
         this.node.isDestroyed = true;
         this.isDestroyed = true;
+    };
+
+    KeyController.prototype.setLabel = function (newLabel) {
+        if (newLabel === this['label' + this.sideShown])
+            return;
+        if (!this['label' + this.sideShown] || !kTransitionDelayKey) {
+            this['label' + this.sideShown] = newLabel;
+            this['label' + this.sideHidden] = newLabel;
+            return;
+        }
+
+        this['label' + this.sideHidden] = newLabel;
+        this.node$.removeClass('keylabelShow' + this.sideShown) .addClass('keylabelShow' + this.sideHidden);
+        this.sideShown = this.sideHidden;
+        return; // xxx
+
+        if (this.labelIsChanging) {
+            if (this.nextLabel) this.nextLabel = newLabel;
+            else this.label = newLabel;
+            return;
+        }
+
+        this.nextLabel = newLabel;
+        this.labelIsChanging = true;
+        var self = this;
+
+        function onTransitionEnd() {
+            if (self.nextLabel) {
+                self.name === 'Slash' && console.log('transition ending', self.labelNode.innerHTML);
+                self.label = self.nextLabel;
+                self.nextLabel = null;
+                self.labelNode$.removeClass('labelChangeStarting') .addClass('labelChangeEnding');
+            } else {
+                self.name === 'Slash' && console.log('transition done', self.labelNode.innerHTML);
+                self.labelNode.removeEventListener(kTransition.endEvent, onTransitionEnd, false);
+                //self.labelNode$.removeClass('labelChangeEnding');
+                self.labelIsChanging = false;
+            }
+        }
+
+        this.name === 'Slash' && console.log('transition starting', this.labelNode.innerHTML);
+        this.labelNode.addEventListener(kTransition.endEvent, onTransitionEnd, false);
+        this.labelNode.style[kTransition.delay] = Math.floor(Math.random() * 250) + 'ms';
+        this.labelNode$.removeClass('labelChangeEnding').addClass('labelChangeStarting');
     };
 
     KeyController.prototype.keyStateDidChange = function (state) {
@@ -84,33 +151,9 @@ module.define('keyController', function (require, exports) {
 
     KeyController.prototype.rankDidChange = function () {
         var r = this.key.rank, mr = this.model.maxRank, l = gradient.length - 1;
-        this.labelNode.style.backgroundColor = gradient[Math.round(l * r / mr)];
-    };
-
-    KeyController.prototype.labelDidChange = function () {
-        if (!this.didSeeFirstLabel) {
-            this.didSeeFirstLabel = true;
-            return;
-        }
-
-        var node = this.labelNode, node$ = this.labelNode$, name = this.name;
-
-        function onTransitionEnd() {
-            console.log(name + ' transition end');
-            node$.removeClass('labelChanged');
-            node.removeEventListener('webkitTransitionEnd', onTransitionEnd, false);
-            node.removeEventListener('mozTransitionEnd', onTransitionEnd, false);
-            node.removeEventListener('transitionEnd', onTransitionEnd, false);
-        }
-
-        console.log(name + ' transition start');
-        node.addEventListener('webkitTransitionEnd', onTransitionEnd, false);
-        node.addEventListener('mozTransitionEnd', onTransitionEnd, false);
-        node.addEventListener('transitionEnd', onTransitionEnd, false);
-        node.style.webkitTransitionDelay = Math.floor(Math.random() * 400) + 'ms';
-        node.style.mozTransitionDelay = Math.floor(Math.random() * 400) + 'ms';
-        node.style.transitionDelay = Math.floor(Math.random() * 400) + 'ms';
-        node$.addClass('labelChanged');
+        var color = gradient[Math.round(l * r / mr)];
+        this.keylabelFront.style.backgroundColor = color;
+        this.keylabelBack.style.backgroundColor = color;
     };
 
 });
